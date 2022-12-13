75783af07541e469d251ce54bc3c6888df7824e1
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-13 18:16:46 +0000

    Fixed deconv

--- VAE.py
+++ VAE.py
@@ -65,7 +65,7 @@ class Decoder(nn.Module):
         self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens, 
                                                 out_channels=num_hiddens//2,
                                                 kernel_size=4, 
-                                                stride=2, padding=1)
+                                                stride=2)#, padding=1)
 
         self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens//2, 
                                                 out_channels=num_hiddens//2,
