f1ebc174fc692e3185e686f8317191c6ea989056
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-13 16:44:39 +0000

    Fixed representation dim

--- VAE.py
+++ VAE.py
@@ -74,8 +74,8 @@ class Decoder(nn.Module):
 
         self._conv_trans_3 = nn.ConvTranspose2d(in_channels=num_hiddens//2, 
                                                 out_channels=out_channels,
-                                                kernel_size=4, 
-                                                stride=2, padding=1)
+                                                kernel_size=3, 
+                                                stride=1, padding=1)
 
     def forward(self, inputs):
         x = self._conv_1(inputs)
